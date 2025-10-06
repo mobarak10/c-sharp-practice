8f6abab1f35a597e6262b51bb6e1a95ddc2540cc
Mobarak Hossain <47492246+mobarak10@users.noreply.github.com>
2025-10-06 09:49:44 +0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+class Test
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("My Name Is Mobarak Hossain. \n I am 30 years old. \n I am a full-stack developer");
+    }
+}
